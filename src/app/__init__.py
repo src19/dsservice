af5daf801744202deb4235e23d0c8b446d91e0ee
src19 <sr3243@srmist.edu.in>
2025-03-04 00:32:53 +0530

    Changes for dockerizing

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import request,jsonify
-from service.messageService import MessageService
+from .service.messageService import MessageService
 from kafka import KafkaProducer
 import json
 import os
