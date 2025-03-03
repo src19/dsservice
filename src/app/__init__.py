57485175ba8ac43286dc7a002b6de0a3a0fa924a
src19 <sr3243@srmist.edu.in>
2025-03-04 00:24:56 +0530

    Add serialize method and local host

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -3,20 +3,25 @@ from flask import request,jsonify
 from service.messageService import MessageService
 from kafka import KafkaProducer
 import json
+import os
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 messageService = MessageService()
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+kafka_host = os.getenv('KAFKA_HOST', 'localhost')
+kafka_port = os.getenv('KAFKA_PORT', '9092')
+kafka_bootstrap_servers = f"{kafka_host}:{kafka_port}"
+print("Kafka server is "+kafka_bootstrap_servers)
+print("\n")
+producer = KafkaProducer(bootstrap_servers=kafka_bootstrap_servers,
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 @app.route('/v1/ds/message/', methods=['POST'])
 def handle_message():
     message = request.json.get('message') #Taking key of message which will contain the actual message
     result = messageService.process_message(message)
-    serialized_result = result.json()
-
+    serialized_result = result.serialize()
     producer.send('expense_service', serialized_result)
 
     return jsonify(result)
