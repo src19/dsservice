0493eb157065e0a38d48eb06b47459c7cc4d52cd
src19 <sr3243@srmist.edu.in>
2025-03-04 00:11:12 +0530

    Add producer side

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,16 +1,24 @@
 from flask import Flask
 from flask import request,jsonify
 from service.messageService import MessageService
+from kafka import KafkaProducer
+import json
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 messageService = MessageService()
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 @app.route('/v1/ds/message/', methods=['POST'])
 def handle_message():
     message = request.json.get('message') #Taking key of message which will contain the actual message
     result = messageService.process_message(message)
+    serialized_result = result.json()
+
+    producer.send('expense_service', serialized_result)
+
     return jsonify(result)
 
 @app.route('/',methods=['GET'])
